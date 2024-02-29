9d47ae4a9d550ace1107de787a952ee257a224f1
TomasMatarazzo <tomasm2008@hotmail.es>
2024-02-29 17:06:24 -0300

    a

--- src/sections/IntegranteEmpresa2.jsx
+++ src/sections/IntegranteEmpresa2.jsx
@@ -27,7 +27,7 @@ const IntegranteEmpresa2 = ({nombre,imagen,descripcion,titulo,bg = "white"}) => 
   return (
     <section className="flex flex-col justify-center max-md:px-0 px-[100px] bg-[#f1f4f6]">
         <section className="flex relative max-md:flex-col rounded-[2%] ">
-            <div className = {`flex font-extralight h-full w-full max-md:flex-col max-md:items-center max-md:align-middle flex flex-row py-12 max-md:py-10 rounded-lg justify-evenly`}>
+            <div className = {`flex font-extralight h-full w-full max-md:flex-col-reverse max-md:items-center max-md:align-middle flex flex-row py-12 max-md:py-10 rounded-lg justify-evenly`}>
                 <div className='w-1/3  flex flex-col justify-center max-md:items-center max-md:text-center max-md:w-[300px]'>
                     <h1 className={`${text1[bg]} py-2 font-bold  leading-10 text-[40px] max-md:text-[32px] `}>{nombre}</h1>
                     <h2 className={`${text1[bg]} py-2 font-semibold  leading-8 text-3xl text-gray-400 max-md:text-[32px] `}>{titulo}</h2>
