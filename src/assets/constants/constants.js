2753802e8b71e650da58178f23dd3c47946ae2db
TomasMatarazzo <tomasm2008@hotmail.es>
2024-02-28 23:54:52 -0300

    Update constants.js

--- src/assets/constants/constants.js
+++ src/assets/constants/constants.js
@@ -1,25 +1,25 @@
 const productos = [
     {
-        nombre:"COCINAS",
+        nombre:"PASTUROMETRO",
         descripcion:"Ofrecemos cocinas, caracterizadas por su durabilidad, máxima potencia y seguridad, con las cuales podrás elaborar los platos más deliciosos a lo largo de los años, de generación en generación.",
         imagen:"https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80",
         boton:"Nuestros cocinas"
     
     },
     {
-        nombre:"CALEFACTORES",
+        nombre:"PASTECH SATELITAL",
         descripcion:"Nuestros calefactores son de bajo consumo y el calor se distribuye de manera óptima. Te vas a olvidar del frío, disfrutando de la calidez en todo el ambiente.",
         imagen:"https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80",
         boton:"Nuestros calefactores"
     },
     {
-        nombre:"CALEFONES",
+        nombre:"PASTECH DRON",
         descripcion:"Equipos de gran rendimiento, con los que podrás regular exactamente la temperatura. Todos nuestros calefones ofrecen máxima seguridad, algunos de los cuales tienen la posibilidad de ser instalados en baños.",
         imagen:"https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80",
         boton:"Nuestros calefones"
     },
     {
-        nombre:"TERMOTANQUES",
+        nombre:"SERVICIOS",
         descripcion:"Modelos tanto a gas como eléctricos, de conexión superior e inferior, todos de prolongada vida útil. Y destacamos la nueva línea de termotanques eléctricos que ofrecen la posibilidad de instalarse tanto vertical como horizontalmente.",
         imagen:"https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80",
         boton:"Nuestros termotanques"
