63d05160f03c64777768fb5df61307af187237b1
TomasMatarazzo <tomasm2008@hotmail.es>
2024-03-01 13:49:57 -0300

    Update Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,10 +1,20 @@
 import LiFooter from "./LiFooter"
 import { productosLinks } from "../assets/constants/constants"
+import { Link } from "react-router-dom"
 
 const Footer = () => {
   return (
     <section className=" w-3/5 m-auto flex relative pb-6 max-md:flex-col ">
-      <LiFooter elementos={["Home","Empresa"]} titulo = "Pastech"></LiFooter>
+      <ul className=" flex flex-col justify-start items-start w-1/3">
+        <h1 className="text-white text-2xl font-normal py-3">Pastech</h1>
+        <Link to = '/'>
+            <li key = {1} className="py-0 my-2 text-lg font-thin text-[rgb(135,147,145)] hover:text-white hover:border-white cursor-pointer border-b-2 border-transparent">Home</li>
+        </Link>
+        <Link to = '/empresa'>
+            <li key = {2} className="py-0 my-2 text-lg font-thin text-[rgb(135,147,145)] hover:text-white hover:border-white cursor-pointer border-b-2 border-transparent">Empresa</li>
+        </Link>       
+
+      </ul>
       <LiFooter elementos={productosLinks} titulo = "Productos y Servicios" ></LiFooter>
       <LiFooter elementos={["Preguntas Frecuentes","Como comprar","Contacto"]} titulo = "Soporte"></LiFooter>
   </section>
