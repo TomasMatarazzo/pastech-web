bbb3074eb63bd29198bcc263a215b4b3fbd18730
TomasMatarazzo <tomasm2008@hotmail.es>
2024-03-01 13:58:31 -0300

    Update Landing.jsx

--- src/sections/Landing.jsx
+++ src/sections/Landing.jsx
@@ -54,7 +54,7 @@ const Landing = () => {
       <div className='group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
         <SlArrowRight onClick={decreaseIndex} size={30} />
       </div>
-        <div className = "absolute top-2/3 left-[823px] border-solid border-b-8 border-t-green text-[#fff] font-[Lato] z-5 min-w-64 max-w-min whitespace-normal text-[70px] leading-[70px] opacity-1 font-extrabold cursor-pointer transition-none ease">{slides[index].texto}</div>
+        <div className = "absolute max-md:top-2/3 max-md:left-10 max-md:text-2xl max-md:border-0 top-2/3 left-[823px] border-solid border-b-8 border-t-green text-[#fff] font-[Lato] z-5 min-w-64 max-w-min whitespace-normal text-[70px] leading-[70px] opacity-1 font-extrabold cursor-pointer transition-none ease">{slides[index].texto}</div>
     </section>
   )
 }
