5f06565b8867b9d4160bcc39647263cd85acf96a
TomasMatarazzo <tomasm2008@hotmail.es>
2024-03-01 23:47:15 -0300

    jj

---
+++ src/components/VideoPlayer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactPlayer from "react-player"
+
+const VideoPlayer = () => {
+
+
+  return (
+//     <div>
+//         <h2></h2>
+//         <video id = "vid" src={video}  loop="true" autoplay="autoplay" muted width="800" height="600" controls />
+//    </div>
+    <div >
+      <ReactPlayer
+        url="https://player.vimeo.com/video/918543074?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" 
+        frameborder="0" controls loop autoplay volume muted playing ={true}
+        width={800}
+        height={600}
+      />
+    </div>
+  )
+}
+
+export default VideoPlayer
