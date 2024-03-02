ba5f666986da6df1dca39c809620b083fc807c0a
TomasMatarazzo <tomasm2008@hotmail.es>
2024-03-02 01:21:24 -0300

    Update VideoPlayer.jsx

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -13,11 +13,7 @@ const VideoPlayer = () => {
 //         <video src="youtu.be/MLeIBFYY6UY" controls="false"></video>
 //    </div>
     <div>
-        <h2></h2>
-
         <video id = "vid" src={pasto}  loop="true" autoplay="autoplay" muted width="800" height="600" controls />
-
-        <video src="youtu.be/MLeIBFYY6UY" controls="false"></video>
    </div>
   )
 }
