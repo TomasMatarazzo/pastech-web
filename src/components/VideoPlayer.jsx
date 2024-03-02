8d27b3150ee4d9eded18fe4af3f6b8011a7802a0
TomasMatarazzo <tomasm2008@hotmail.es>
2024-03-02 01:21:08 -0300

    a ver che

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactPlayer from "react-player"
+import image from "../assets/images/01.jpg"
+import pasto from "../assets/images/pasto.mp4"
+
 
 const VideoPlayer = () => {
 
@@ -7,16 +10,15 @@ const VideoPlayer = () => {
   return (
 //     <div>
 //         <h2></h2>
-//         <video id = "vid" src={video}  loop="true" autoplay="autoplay" muted width="800" height="600" controls />
+//         <video src="youtu.be/MLeIBFYY6UY" controls="false"></video>
 //    </div>
-    <div >
-      <ReactPlayer
-        url="https://player.vimeo.com/video/918543074?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" 
-        frameborder="0" controls loop autoplay volume muted playing ={true}
-        width={750}
-        height={600}
-      />
-    </div>
+    <div>
+        <h2></h2>
+
+        <video id = "vid" src={pasto}  loop="true" autoplay="autoplay" muted width="800" height="600" controls />
+
+        <video src="youtu.be/MLeIBFYY6UY" controls="false"></video>
+   </div>
   )
 }
 
