8ea97056f0d468eba273605cc52b1873cb3d834d
TomasMatarazzo <tomasm2008@hotmail.es>
2024-03-12 23:41:34 -0300

    gato

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom"
 
 const Footer = () => {
   return (
-    <section className=" w-3/5 m-auto flex relative pb-6 max-md:flex-col ">
-      <ul className=" flex flex-col justify-start items-start w-1/3">
+    <section className=" w-3/5 m-auto flex relative pb-6 max-md:flex-col max-md:items-center">
+      <ul className=" flex flex-col justify-start items-start w-1/3 max-md:w-[90%] max-md:items-center">
         <h1 className="text-white text-2xl font-normal py-3">Pastech</h1>
         <Link to = '/'>
             <li key = {1} className="py-0 my-2 text-lg font-thin text-[rgb(135,147,145)] hover:text-white hover:border-white cursor-pointer border-b-2 border-transparent">Inicio</li>
