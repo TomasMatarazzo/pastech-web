21d932821cdeed7f2501dba59c52082691d1ab29
TomasMatarazzo <tomasm2008@hotmail.es>
2024-03-19 01:29:41 -0300

    Update Carta.jsx

--- src/pages/Subscripcion/Carta.jsx
+++ src/pages/Subscripcion/Carta.jsx
@@ -5,7 +5,7 @@ const Carta = ({titulo,descripcion,precio,gratis}) => {
   return (
     <div className='w-[18vw] max-md:w-[80vw] max-md:my-8 max-md:mx-18 max-md:h-[50vh] h-[60vh] bg-s-green rounded-md flex flex-col items-center justify-between text-center'>
         <div>
-          <h1 className='font-bold text-3xl text-t-green py-8'>{titulo}</h1>
+          <h1 className='font-bold text-3xl text-t-green py-2'>{titulo}</h1>
           <p className='font-semibold text-lg px-4 text-t-green whitespace-pre-line'>{descripcion}</p>
         </div>
         <div className='w-[100%] h-[30%] flex flex-col justify-end items-center'>
@@ -14,7 +14,7 @@ const Carta = ({titulo,descripcion,precio,gratis}) => {
                 alt = "Logo"
                 width = {260}
                 height={90}
-                className=' max-lg:w-[200px] w-[150px] max-md:w-[100px] self-center'
+                className=' w-[100px] max-md:w-[100px] self-center'
           ></img>
             <button className='px-8 py-2 my-4 bg-first-green rounded-md font-bold text-xl text-white cursor-pointer hover:bg-t-green z-20'>CONTRATAR</button>
             { gratis ?
