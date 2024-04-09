f28dad7562df106f3c20f9552f3406d1900314e5
TomasMatarazzo <tomasm2008@hotmail.es>
2024-04-08 23:30:53 -0300

    aa

--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Titulo = ({titulo,bg,padding}) => {
   return (
     <section className={`flex justify-center items-center xl:padding-1  mt-[40px] max-md:pt-[40px] max-md:text-center ${bg} ${padding}`}>
-    <div className={`flex relative  justify-center items-center flex-col rounded-[2%] p-16 pb-4 ${padding}`}>
-        <h1 className= {` border-b-[2px] border-dashed border-first-green inline-block py-2  pb-0 text-4xl leading-[56px] max-md:text-[32px] font-bold text-first-green font-[Roboto]`}>{titulo}</h1>
+    <div className={`flex relative  justify-center items-center flex-col rounded-[2%] p-16 max-md:py-16 pb-4 ${padding}`}>
+        <h1 className= {` border-b-[2px] border-dashed border-first-green inline-block py-2  pb-0 text-4xl leading-[56px] max-md:leading-[40px] max-md:text-[32px] font-bold text-first-green font-[Roboto]`}>{titulo}</h1>
     </div>
 </section>
   )
