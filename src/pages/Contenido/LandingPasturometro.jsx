f170e875f05fe41f8fa43a733c2f480fe0f91fed
TomasMatarazzo <tomasm2008@hotmail.es>
2024-04-08 23:13:35 -0300

    a

--- src/pages/Contenido/LandingPasturometro.jsx
+++ src/pages/Contenido/LandingPasturometro.jsx
@@ -11,7 +11,7 @@ const LandingPasturometro = ({imagen,texto}) => {
           <div className=" max-md:px-0 w-[20vw]  max-md:w-full flex relative flex-row  justify-center items-start rounded-[2%] max-md:m-0 max-md:flex-col max-md:justify-center ">
             <img src={vista1} alt="Whatsapp" className='z-30 max-h-80 self-center' />         
           </div>
-          <div className=' w-[50vw] max-md:mt-10 flex flex-col justify-center max-md:items-center max-md:text-center max-md:w-full px-8 '>
+          <div className=' w-[50vw] max-md:mt-10 flex flex-col justify-center max-md:items-center max-md:text-center max-md:w-full px-8 max-md:px-0 '>
             <p className = {`py-9 font-normal text-xl`}>Es un dispositivo <strong>inteligente</strong> de medición de altura comprimida de <strong>pasto</strong>, que mediante lecturas electrónicas <strong>georeferenciadas</strong> y <strong>algoritmos integrados</strong> a una aplicación del <strong>teléfono móvil</strong>, estima de forma confiable y en <strong>tiempo real</strong> la cantidad de pasto que hay disponible para los animales en cada <strong>zona</strong> o <strong>potrero</strong> del campo. <br></br><br></br>Su diseño <strong>ergonómico</strong> facilita la manipulación del equipo y una interfaz amigable de la <strong>App</strong> simplifica la interpretación de los datos para la toma de decisiones en cuanto al manejo de la pastura y los animales.<br></br> Asimismo, es un producto que incorpora tecnología de <strong>sensores</strong> que permite utilizarse adecuadamente sin necesidad de cables ni funcionamientos mecánicos más propensos al desgaste.</p>
           </div>
           <div className=" w-[20vw] max-md:px-0"></div>
--- src/pages/PastechSatelital/LandingSatelital.jsx
+++ src/pages/PastechSatelital/LandingSatelital.jsx
@@ -10,7 +10,7 @@ const LandingSatelital = ({imagen,texto}) => {
                             <img src={satelite} alt="Whatsapp" className='z-30 self-center' />            
         </div>
 
-        <div className='w-[50vw] max-md:mt-10 flex flex-col justify-center max-md:items-center max-md:text-center max-md:w-full px-8'>
+        <div className='w-[50vw] max-md:mt-10 flex flex-col justify-center max-md:items-center max-md:text-center max-md:w-full px-8  max-md:px-0'>
             <p className = {`py-9 font-normal text-xl`}><strong>Pastech Satelital</strong> es una <strong>aplicación</strong> de <strong>acceso web</strong> que permite determinar múltiples <strong>indicadores</strong> vinculados con el <strong>pasto</strong> y <strong>administrar</strong> varios potreros. Esta aplicación permite <strong>generar</strong> <strong>predicción inteligente</strong></p>
             <p className = {`py-9 font-normal text-xl text-[#626262]`}>Mediante la integración de datos de <strong>sensores remotos</strong> montados en <strong>satélites</strong> junto con modelos <strong>biofísicos clima-dependientes</strong> e <strong>inteligencia artificial</strong>, logramos estimar con precisión las variaciones espaciales y temporales del crecimiento y disponibilidad de las pasturas en relación a las condiciones edafo-climáticas de cada sitio en particular. En este sentido, nuestra herramienta de base satelital representa una <strong>solución confiable</strong> para las estimaciones de disponibilidad de pasto en el campo de forma remota, proporcionando así predicciones para guiar proactivamente las recomendaciones de manejo del pastoreo.</p>
         </div>
