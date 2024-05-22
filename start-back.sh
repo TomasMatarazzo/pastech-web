3bbb5773b66ed0fdb91b4e9c08fd03c1bb53c213
TomasMatarazzo <tomasm2008@hotmail.es>
2024-05-22 14:13:22 -0300

    probando

--- start-back.sh
+++ start-back.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+
+
 git pull
+nvm use 16.20.1
 # Detener el proceso existente si está en ejecución
 pm2 stop server
 
